7db65bf362facca51980dbccbe9d690b90192987
anon_real <anon_real_anon@protonmail.com>
2023-10-11 14:53:13 -0700

    AC contracts

--- src/main/scala/Contracts.scala
+++ src/main/scala/Contracts.scala
@@ -3,17 +3,17 @@ object Contracts {
   val buyBack: String =
     """{
       |  // we could pay less fee (e.g., 1e6) and add the rest to AC box to ensure it can create auction boxes
-      |  val MaxMinerFee = 10000000L
+      |  val MaxMinerFee = MAX_MINER_FEE
       |
-      |  val teamFee = 2 // in percent
-      |  val lpBox = INPUTS(0)
+      |  val teamFee = TEAM_FEE // in percent
+      |  val lpBox = if (INPUTS.size > 2) INPUTS(0) else CONTEXT.dataInputs(0)
       |  val acBox = INPUTS(1)
       |
       |  val successfulAuction = SELF.tokens.size == 0
       |
       |  // OUTPUTS(0) is LP box
       |  val outAc = OUTPUTS(1)
-      |  val teamOut = OUTPUTS(2)
+      |  val teamOut = OUTPUTS.getOrElse(2, OUTPUTS(0))
       |
       |  val NFT = acBox.tokens(0)._1
       |  val ac = acBox.tokens(1)
@@ -22,13 +22,14 @@ object Contracts {
       |
       |  // AMM swap
       |  val fundInp = SELF.value - MaxMinerFee - (SELF.value * teamFee) / 100
-      |  val xRes = lpBox.value
-      |  val yRes = acBox.tokens(2)._2
+      |  val yRes = lpBox.value
+      |  val xRes = lpBox.tokens(2)._2
       |  val feeCoef = lpBox.R4[Int].get
-      |  val willGet = (xRes * fundInp * feeCoef) / (yRes * 1000L + fundInp * feeCoef)
+      |  //val willGet = (xRes * fundInp * feeCoef) / (yRes * 1000L + fundInp * feeCoef)
+      |  val willGet = (xRes.toBigInt * fundInp.toBigInt * feeCoef) / (yRes.toBigInt * 1000L + fundInp.toBigInt * feeCoef)
       |
       |  val rightOutAc = outAc.tokens(0)._1 == NFT && outAc.tokens(1)._2 >= ac._2 + willGet
-      |  val rightLpBox = lpBox.tokens(2)._1 == CONFIG_LP
+      |  val rightLpBox = lpBox.tokens(0)._1 == CONFIG_LP
       |
       |  val rightTeamOut = teamOut.value >= (SELF.value * teamFee) / 100
       |  val rightTeamBox = teamOut.propositionBytes == TEAM_ADDRESS && rightTeamOut
@@ -37,11 +38,11 @@ object Contracts {
       |
       |  // we return the tokens to AC box
       |  val failedAuction = {
-      |    val toReturn = SELF.tokens(0)
+      |    val toReturn = SELF.tokens.getOrElse(0, (Coll[Byte](), 0L))
       |    val rightTok = toReturn._1 == acBox.tokens(1)._1
-      |    val addToAc = OUTPUTS(0).tokens(0)._1 == NFT && OUTPUTS(0).tokens(1)._2 == ac._2 + toReturn._2 &&
+      |    val addToAc = OUTPUTS(0).tokens(0)._1 == NFT && OUTPUTS(0).tokens(1)._2 >= ac._2 + toReturn._2 &&
       |                   OUTPUTS(0).value >= acBox.value + SELF.value - MaxMinerFee
-      |    rightTok && addToAc && INPUTS.size == 2 && !successfulAuction
+      |    rightTok && addToAc && INPUTS.size == 2 && !successfulAuction && INPUTS.size == 2
       |  }
       |
       |  sigmaProp((buyBack || failedAuction) && rightAcBox)
@@ -58,12 +59,13 @@ object Contracts {
       |  // we start decreasing price auction. The auction price will start from coef * LP price and will decrease to
       |  // LP price in the auction period. Coef could be set to 2 for example. This makes price discovery much easier!
       |  // R5: Coll[Coll[Long]] - Auction info: [numCoinsToAuction, period, coef]
-      |  val MaxMinerFee = 10000000L
-      |  val auctionBoxInitialVal = 1000000L
+      |  val MaxMinerFee = MAX_MINER_FEE
+      |  val auctionBoxInitialVal = AUC_BOX_INIT_VAL
+      |
       |  val HOUR = 3600000L
       |  val curTime = CONTEXT.preHeader.timestamp
       |
-      |  val lpBox = CONTEXT.dataInputs(1) // to get the price
+      |  val lpBox = CONTEXT.dataInputs(0) // to get the price
       |
       |  val prevTime = SELF.R4[Coll[Long]].get(0) // last time we have started auctions
       |  val frequency = SELF.R4[Coll[Long]].get(1)
@@ -83,9 +85,10 @@ object Contracts {
       |    val period = auction(1)
       |    val coef = auction(2) // (coef is 1000 / actual coef)
       |
-      |    val aucOut = OUTPUTS(ind + 1)
+      |    val aucOut = OUTPUTS.getOrElse(ind + 1, INPUTS(0))
       |    val rightContract = blake2b256(aucOut.propositionBytes) == AuctionContractHash
-      |    val rightTokens = aucOut.tokens(0)._1 == coinId && aucOut.tokens(0)._2 == numToAuction
+      |    val aucToken = aucOut.tokens.getOrElse(0, (Coll[Byte](), 0L))
+      |    val rightTokens = aucToken._1 == coinId && aucToken._2 == numToAuction
       |    val aucSt = curTime + (HOUR/2)
       |    val aucEnd = aucSt + period
       |    val lpErg = lpBox.value
@@ -96,7 +99,8 @@ object Contracts {
       |    val amountDecrease = (stPrice - lpPrice) / numDecrease
       |
       |    val rightRegisters = blake2b256(aucOut.R4[Coll[Byte]].get) == BuyBackContractHash &&
-      |                           aucOut.R5[(Long, Long)].get == (aucSt, aucEnd) &&
+      |                           aucOut.R5[(Long, Long)].get._1 >= aucSt - HOUR / 2 && aucOut.R5[(Long, Long)].get._1 <= aucSt + HOUR / 2 &&
+      |                           aucOut.R5[(Long, Long)].get._2 >= aucEnd - HOUR / 2 && aucOut.R5[(Long, Long)].get._2 <= aucEnd + HOUR / 2 &&
       |                           aucOut.R6[Coll[Long]].get == Coll(stPrice, -amountDecrease, HOUR) &&
       |                           aucOut.R7[Coll[Byte]].get == Coll[Byte]()
       |    rightContract && rightTokens && rightRegisters && aucOut.value == auctionBoxInitialVal
@@ -110,22 +114,22 @@ object Contracts {
       |  val rightRegisters = OUTPUTS(0).R4[Coll[Long]].get == Coll(prevTime + frequency, frequency) &&
       |                       OUTPUTS(0).R5[Coll[Coll[Long]]] == SELF.R5[Coll[Coll[Long]]]
       |
-      |  // in case of failed auctions, we get back the tokens
+      |  // in case of failed auctions, we get back the tokens or in case of successful auctions we buy back the tokens
       |  val sameScript = OUTPUTS(0).propositionBytes == SELF.propositionBytes
       |  val allowAddingToken = {
-      |    val selfOut = OUTPUTS(0)
+      |    val selfOut = if (INPUTS.size == 2) OUTPUTS(0) else OUTPUTS(1)
       |    val okayTokens = selfOut.tokens(0) == NFT && selfOut.tokens(1)._1 == coinId && selfOut.tokens(1)._2 >= coinAm
       |    val keepRegisters = selfOut.R4[Coll[Long]] == SELF.R4[Coll[Long]] &&
       |                         selfOut.R5[Coll[Coll[Long]]] == SELF.R5[Coll[Coll[Long]]]
       |    val sameVal = selfOut.value >= SELF.value // allows adding ERGs to ensure auction creation
-      |    okayTokens && keepRegisters && sameVal
+      |    okayTokens && keepRegisters && sameVal && selfOut.propositionBytes == SELF.propositionBytes
       |  }
       |
-      |  val rightLpBox = lpBox.tokens(2)._1 == CONFIG_LP
+      |  val rightLpBox = lpBox.tokens(0)._1 == CONFIG_LP
       |  val minerFeeOkay = OUTPUTS(OUTPUTS.size - 1).value <= MaxMinerFee
       |  val startAuctions = rightLpBox && correctNFT && correctCoin && allOkay && rightRegisters &&
       |                        enoughTimePassed && OUTPUTS.size == auctionInfo.size + 2 && minerFeeOkay
-      |  sigmaProp((startAuctions || allowAddingToken) && sameScript)
+      |  sigmaProp((startAuctions && sameScript) || (allowAddingToken))
       |}""".stripMargin
 
 
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,44 +1,250 @@
+import org.ergoplatform.ErgoBox.R7
+import org.ergoplatform.appkit.impl.ErgoTreeContract
 import org.ergoplatform.{ErgoAddress, ErgoAddressEncoder}
-import org.ergoplatform.appkit.{Address, ConstantsBuilder, ErgoClient, NetworkType, RestApiErgoClient}
-import org.ergoplatform.sdk.{ErgoId, ErgoToken}
+import org.ergoplatform.appkit.{Address, BlockchainContext, ConstantsBuilder, ErgoClient, ErgoProver, ErgoType, ErgoValue, InputBox, NetworkType, OutBox, RestApiErgoClient, TransactionBox}
+import org.ergoplatform.sdk.{ErgoId, ErgoToken, Iso, JavaHelpers}
 import scorex.crypto.hash.Blake2b256
+import sigmastate.Values.ErgoTree
+import sigmastate.eval.Colls
+import special.collection.{Coll, collBuilderRType}
+
+import scala.math.BigDecimal.long2bigDecimal
+import scala.collection.JavaConverters._
 
 object Main {
-  val ergoClient: ErgoClient = RestApiErgoClient.create("http://95.217.135.15:9053/", NetworkType.MAINNET, "", "https://api.ergoplatform.com/api/v1/")
+  val ergoClient: ErgoClient = RestApiErgoClient.create("http://128.140.55.130:9053/", NetworkType.MAINNET, "", "https://api.ergoplatform.com/api/v1/")
   val addrEnc = new ErgoAddressEncoder(NetworkType.MAINNET.networkPrefix)
 
- // for testing
+  // for testing
   val USER_P2PK_ADDRESS: ErgoAddress = Address.create("9gN7YssQgnRzDDXHAuamAwgikRkzeLYq8Jn9YvsPcKD13ajj33u").getErgoAddress
   val USER_SECRET = "19899857734851286010002701428515536889175392016268936110507684368097105859366"
 
+  var buyBackAddr: ErgoAddress = _
+  var acAddr: ErgoAddress = _
 
-  val mainContract: String = Contracts.main;
+  val acId = "aee8132a6602dd215dac8d1caf973581277614e267702a770f45d7ffe5234cba"
+  val lpnft = ErgoId.create("915da2ac421906919351163a9afa1f17918272750987906b00247e91925e757d")
+  val teamAddr = addrEnc.fromString("9evruWoFfhTZ4q4Xc4whQK4bJMqMAF7z9YeygzLvCHMgR17Pt5n").get
+  val acNFT = ErgoId.create("02194927b8d96833067052f40f032039d55c9d9f87a2ca47b7e4d56497de0f58")
+  val auctionAddr = addrEnc.fromString("quE2HvwgAp7z8q4tD1BfCfj72TPziUHqruk3L8jUWQ7bWGD7ugdkpkgYx9kztPTmJborN4baTrHuenaZrCtoB3VHeLjQUQACzZ67qpnMGUAyGs93mUpwodKekvSM8Y6rCFVMxFw76MD8ZUjMPd2J8awcfECurL9v6HkjFnzKJr72NXdQ18XYYGRneXbbkgU5qzUKTLoC2weUGsDpNhfDh9xBGV7CYconikP79c9b3ZcquSBdNCu4G5r2uvBVTHXaFgk5TioeSL4SqwS5sugeEeicTPre842uZBzNd7B3XqE75Rjum1bz1vBgGtadMqt2XRui7tqaK44LjeYfds3N4CWZsYwx69h2PpHFrU9Mzvvj3ju6XGVS5X6GnyGgttXiHgokZPhD45m2etzAUXovNPeutrw5zmQudCh9jXBSfujXDBPS7ohC7qLj3Hj4zfRJTmUzPNMEUGLED57kowMXdAq95h4LciSLcNgZjM3dAwVkkPFgTnu1gvFSR6HAb78d8cx4khpf5bvtPpRxLk1vXeqRxBagUQwgaTBJUEAbxF9bfs7TQBzboKL5krjpqXKydYQb5pbZ64bRk6U6hzNjmn8XEexRE8RACaJrk9MvtXVVqJRyKRkWRvMKgo3QtfXR1qNZcXKV5AHzwCPfHXfxXAkSkEPPXFxYyj33KLuuLAw7LyWqL1v6fFXzohq1oZB3tqXGZbksri3i86cCtMoUFUCUHT2BgCxUmQVGnuUvNTe69PU5VKraGowqUo8MJFXS7njsW7uZyK867qZpPPXwu2fLgK2whDihx6JKY1Ub3aw3mUkjbdqhMxSh4m6ZhD2piJYwtiEb7juZhrhVWVqaJDFCc2aED8Vdc1Z4W9pEt8UKhHcLcHn6CiaSHiyhFwRY3bMuVZwtY2XFwzQRSfW2VyYoC5Dqby43xYuEHe1Ld7ANMsXgHzwwkisdt4rQ1EHMynXM6926DEVYewgxrGEaXdYDymLWZ5DKHdmyvYW6keH9NysQGKytiax8wUyiqyaLhXoqbJCWxrcyu9MZLZpJFRXonKCNK1SAmRkRNQfkmY9jDVnj5nBdjtEGxa9pwAFENv6xa58J1zcA4MD6Tj9B4B36YrWT2TLs7Pp4jMJryA5sUVsL6jn7cEnbjaH78qQw2sicncneBaAoQnFJedyWfnQC3GYRAfRPp3kxfZNsJf2aQQ").get
+  val HOUR: Long = 60 * 60 * 1000L
+  var prover: ErgoProver = _
+  val MAX_MINER_FEE = 1e7.toLong
+  val TEAM_FEE = 2
+  val AUC_BOX_INIT_VAL = 1e7.toLong
+
+  def compileContracts(ctx: BlockchainContext): Unit = {
+    val auctionHash = Blake2b256.hash(auctionAddr.script.bytes)
+
+    val buyBackContract = ctx.compileContract(ConstantsBuilder.create()
+      .item("CONFIG_LP", lpnft.getBytes)
+      .item("TEAM_ADDRESS", teamAddr.script.bytes)
+      .item("ACNFT", acNFT.getBytes)
+      .item("MAX_MINER_FEE", MAX_MINER_FEE)
+      .item("TEAM_FEE", TEAM_FEE)
+      .build(), Contracts.buyBack)
+    buyBackAddr = addrEnc.fromProposition(buyBackContract.getErgoTree).get
+    println("Buy back contract address: " + addrEnc.fromProposition(buyBackContract.getErgoTree).get)
+
+    val buyBackContractHash = Blake2b256.hash(buyBackContract.getErgoTree.bytes)
+    var contract = ctx.compileContract(ConstantsBuilder.create()
+      .item("CONFIG_LP", lpnft.getBytes)
+      .item("AuctionContractHash", auctionHash)
+      .item("BuyBackContractHash", buyBackContractHash)
+      .item("MAX_MINER_FEE", MAX_MINER_FEE)
+      .item("AUC_BOX_INIT_VAL", AUC_BOX_INIT_VAL)
+      .build(), Contracts.main)
+    acAddr = addrEnc.fromProposition(contract.getErgoTree).get
+    println("Main contract address: " + addrEnc.fromProposition(contract.getErgoTree).get)
+
+  }
+
+  def getUserBox(ctx: BlockchainContext, tokens: List[ErgoToken] = List.empty, value: Long = 1e9.toLong, script: ErgoTree = USER_P2PK_ADDRESS.script): InputBox = {
+    val txB = ctx.newTxBuilder()
+    val out = txB.outBoxBuilder()
+      .contract(new ErgoTreeContract(script, NetworkType.MAINNET))
+      .value(value)
+      .build()
+    out.convertToInputWith("f36a52dd37aaafe8dc601659f610dcf06d7d04ef778f148c9e734d7c50c67840", 0)
+  }
+
+  def getAcBox(ctx: BlockchainContext, auctionInfo: Seq[Seq[Long]], contract: ErgoAddress): InputBox = {
+    val seqColl = auctionInfo.map(r => ErgoValue.of(r.toArray).getValue).toArray
+    val auctions = ErgoValue.of(seqColl, ErgoType.collType(ErgoType.longType()))
+
+    val txB = ctx.newTxBuilder()
+    val out = txB.outBoxBuilder()
+      .contract(new ErgoTreeContract(contract.script, NetworkType.MAINNET))
+      .value(1e9.toLong)
+      .tokens(new ErgoToken(acNFT, 1), new ErgoToken(acId, 100000))
+      .registers(ErgoValue.of(Seq(0L, HOUR).toArray), auctions)
+      .build()
+    out.convertToInputWith("f36a52dd37aaafe8dc601659f610dcf06d7d04ef778f148c9e734d7c50c67840", 0)
+  }
+
+
+  def getBuyBackBox(ctx: BlockchainContext, value:Long=1e9.toLong): InputBox = {
+    val txB = ctx.newTxBuilder()
+    val out = txB.outBoxBuilder()
+      .contract(new ErgoTreeContract(buyBackAddr.script, NetworkType.MAINNET))
+      .value(value)
+      .build()
+    out.convertToInputWith("f36a52dd37aaafe8dc601659f610dcf06d7d04ef778f148c9e734d7c50c67840", 0)
+  }
+
+  def getBuyBackBoxFailed(ctx: BlockchainContext, value: Long = 100): InputBox = {
+    val txB = ctx.newTxBuilder()
+    val out = txB.outBoxBuilder()
+      .contract(new ErgoTreeContract(buyBackAddr.script, NetworkType.MAINNET))
+      .value(1e7.toLong)
+      .tokens(new ErgoToken(acId, value))
+      .build()
+    out.convertToInputWith("f36a52dd37aaafe8dc601659f610dcf06d7d04ef778f148c9e734d7c50c67840", 0)
+  }
+
+  def startAuctionTest(ctx: BlockchainContext): Unit = {
+    val curTime = ctx.createPreHeader().build().getTimestamp
+    val auctionInfo: Seq[Seq[Long]] = Seq(Seq(100L, HOUR * 24L, 500L), Seq(100L, HOUR * 24L, 500L))
+    val cntr = "{sigmaProp(true)}"
+    val compiled = ctx.compileContract(ConstantsBuilder.empty(), cntr)
+    var addr = addrEnc.fromProposition(compiled.getErgoTree).get
+    addr = acAddr
+    val ac = getAcBox(ctx, auctionInfo, addr)
+
+    val lp = ctx.getBoxesById("57533c42c447eac4cff38d7ed8407d3ee85914b3bd1e6daa53332f08ebcd19d0").head
+    val txB = ctx.newTxBuilder()
+    val ins = Seq(ac)
+
+    val minerFee = 1e6.toLong
+    val smAuction = auctionInfo.map(a => a.head).sum
+    val outAc = txB.outBoxBuilder()
+      .contract(new ErgoTreeContract(acAddr.script, NetworkType.MAINNET))
+      .value(ac.getValue - auctionInfo.size * AUC_BOX_INIT_VAL - minerFee)
+      .tokens(ac.getTokens.get(0), new ErgoToken(acId, ac.getTokens.get(1).getValue - smAuction))
+      .registers(ErgoValue.of(Seq(HOUR, HOUR).toArray), ac.getRegisters.get(1))
+      .build()
+    var outs = Seq[OutBox](outAc)
+    val stPrice = lp.getValue / lp.getTokens.get(2).getValue
+    auctionInfo.foreach(info => {
+      val curPrice = stPrice * info.head
+      println(curPrice/1e9)
+      val coef = 1000 / info(2)
+      val curStPrice = curPrice * coef
+      val numDec = info(1) / HOUR
+      val step = (curStPrice - curPrice) / numDec
+
+      val auc = txB.outBoxBuilder()
+        .contract(new ErgoTreeContract(auctionAddr.script, NetworkType.MAINNET))
+        .value(AUC_BOX_INIT_VAL)
+        .tokens(new ErgoToken(acId, info.head))
+        .registers(ErgoValue.of(buyBackAddr.script.bytes), ErgoValue.pairOf(ErgoValue.of(curTime + HOUR / 2), ErgoValue.of(curTime + info(1))),
+          ErgoValue.of(Seq(curStPrice, -step, HOUR).toArray), ErgoValue.of(Seq[Byte]().toArray))
+        .build()
+      outs = outs :+ auc
+    })
+
+    val tx = txB.boxesToSpend(ins.asJava)
+      .addOutputs(outs: _*)
+      .fee(minerFee)
+      .sendChangeTo(teamAddr)
+      .addDataInputs(lp)
+      .build()
+    prover.sign(tx)
+  }
+
+  def returnTest(ctx: BlockchainContext): Unit = {
+    val auctionInfo: Seq[Seq[Long]] = Seq(Seq(100L, HOUR * 24L, 500L), Seq(100L, HOUR * 24L, 500L))
+    val cntr = "{sigmaProp(true)}"
+    val compiled = ctx.compileContract(ConstantsBuilder.empty(), cntr)
+    var addr = addrEnc.fromProposition(compiled.getErgoTree).get
+    addr = acAddr
+    val ac = getAcBox(ctx, auctionInfo, addr)
+    val buyBackBox = getBuyBackBoxFailed(ctx)
+
+    val lp = ctx.getBoxesById("57533c42c447eac4cff38d7ed8407d3ee85914b3bd1e6daa53332f08ebcd19d0").head
+    val txB = ctx.newTxBuilder()
+    val ins = Seq(buyBackBox, ac)
+
+    val minerFee = 1e6.toLong
+    val outAc = txB.outBoxBuilder()
+      .contract(new ErgoTreeContract(acAddr.script, NetworkType.MAINNET))
+      .value(ac.getValue + (buyBackBox.getValue - minerFee))
+      .tokens(ac.getTokens.get(0), new ErgoToken(acId, ac.getTokens.get(1).getValue + buyBackBox.getTokens.get(0).getValue))
+      .registers(ac.getRegisters.asScala.toSeq: _*)
+      .build()
+
+    val tx = txB.boxesToSpend(ins.asJava)
+      .addOutputs(outAc)
+      .fee(minerFee)
+      .sendChangeTo(teamAddr)
+      .addDataInputs(lp)
+      .build()
+    prover.sign(tx)
+  }
+
+
+
+  def buyBackTest(ctx: BlockchainContext): Unit = {
+    val auctionInfo: Seq[Seq[Long]] = Seq(Seq(100L, HOUR * 24L, 500L), Seq(100L, HOUR * 24L, 500L))
+
+
+    val cntr = "{sigmaProp(true)}"
+    val compiled = ctx.compileContract(ConstantsBuilder.empty(), cntr)
+    var addr = addrEnc.fromProposition(compiled.getErgoTree).get
+    addr = acAddr
+    val ac = getAcBox(ctx, auctionInfo, addr)
+    val buyBackBox = getBuyBackBox(ctx)
+
+    val lp = ctx.getBoxesById("57533c42c447eac4cff38d7ed8407d3ee85914b3bd1e6daa53332f08ebcd19d0").head
+    val txB = ctx.newTxBuilder()
+    val ins = Seq(lp, ac, buyBackBox)
+
+    val yRes = lp.getValue.toBigInt
+    val xRes = lp.getTokens.get(2).getValue
+    val lpFee = lp.getRegisters.get(0).getValue.asInstanceOf[Int].toLong
+    val funds = buyBackBox.getValue - MAX_MINER_FEE - (buyBackBox.getValue * TEAM_FEE) / 100
+    val willGet = ((xRes * funds * lpFee) / (yRes * 1000L + funds * lpFee)).toLong
+
+    val minerFee = 1e6.toLong
+    val newAcVal = 1e7.toLong - minerFee + ac.getValue
+    val newAcTokenVal = ac.getTokens.get(1).getValue + willGet
+    val outAc = txB.outBoxBuilder()
+      .contract(new ErgoTreeContract(acAddr.script, NetworkType.MAINNET))
+      .value(newAcVal)
+      .tokens(ac.getTokens.get(0), new ErgoToken(acId, newAcTokenVal))
+      .registers(ac.getRegisters.asScala.toSeq: _*)
+      .build()
+    val outLp = txB.outBoxBuilder()
+      .contract(new ErgoTreeContract(lp.getErgoTree, NetworkType.MAINNET))
+      .value(lp.getValue + funds)
+      .tokens(lp.getTokens.get(0), lp.getTokens.get(1), new ErgoToken(acId, lp.getTokens.get(2).getValue - willGet))
+      .registers(lp.getRegisters.asScala.toSeq: _*)
+      .build()
+
+    val teamOut = txB.outBoxBuilder()
+      .contract(new ErgoTreeContract(teamAddr.script, NetworkType.MAINNET))
+      .value((buyBackBox.getValue * TEAM_FEE) / 100)
+      .build()
+
+    val tx = txB.boxesToSpend(ins.asJava)
+      .addOutputs(outLp, outAc, teamOut)
+      .fee(minerFee)
+      .sendChangeTo(teamAddr)
+      .addDataInputs(lp)
+      .build()
+    prover.sign(tx)
+  }
+
+
   def main(args: Array[String]): Unit = {
     // use appkit to compile the contract
     ergoClient.execute(ctx => {
+      compileContracts(ctx)
+      prover = ctx.newProverBuilder().withDLogSecret(BigInt.apply(USER_SECRET).bigInteger).build()
 
-      val lpnft = ErgoId.create("915da2ac421906919351163a9afa1f17918272750987906b00247e91925e757d")
-      val team = "9evruWoFfhTZ4q4Xc4whQK4bJMqMAF7z9YeygzLvCHMgR17Pt5n"
-      val teamAddr = addrEnc.fromString(team).get
-      val acId = ErgoId.create("02194927b8d96833067052f40f032039d55c9d9f87a2ca47b7e4d56497de0f58")
-
-      val auctionAddr = addrEnc.fromString("quE2HvwgAp7z8q4tD1BfCfj72TPziUHqruk3L8jUWQ7bWGD7ugdkpkgYx9kztPTmJborN4baTrHuenaZrCtoB3VHeLjQUQACzZ67qpnMGUAyGs93mUpwodKekvSM8Y6rCFVMxFw76MD8ZUjMPd2J8awcfECurL9v6HkjFnzKJr72NXdQ18XYYGRneXbbkgU5qzUKTLoC2weUGsDpNhfDh9xBGV7CYconikP79c9b3ZcquSBdNCu4G5r2uvBVTHXaFgk5TioeSL4SqwS5sugeEeicTPre842uZBzNd7B3XqE75Rjum1bz1vBgGtadMqt2XRui7tqaK44LjeYfds3N4CWZsYwx69h2PpHFrU9Mzvvj3ju6XGVS5X6GnyGgttXiHgokZPhD45m2etzAUXovNPeutrw5zmQudCh9jXBSfujXDBPS7ohC7qLj3Hj4zfRJTmUzPNMEUGLED57kowMXdAq95h4LciSLcNgZjM3dAwVkkPFgTnu1gvFSR6HAb78d8cx4khpf5bvtPpRxLk1vXeqRxBagUQwgaTBJUEAbxF9bfs7TQBzboKL5krjpqXKydYQb5pbZ64bRk6U6hzNjmn8XEexRE8RACaJrk9MvtXVVqJRyKRkWRvMKgo3QtfXR1qNZcXKV5AHzwCPfHXfxXAkSkEPPXFxYyj33KLuuLAw7LyWqL1v6fFXzohq1oZB3tqXGZbksri3i86cCtMoUFUCUHT2BgCxUmQVGnuUvNTe69PU5VKraGowqUo8MJFXS7njsW7uZyK867qZpPPXwu2fLgK2whDihx6JKY1Ub3aw3mUkjbdqhMxSh4m6ZhD2piJYwtiEb7juZhrhVWVqaJDFCc2aED8Vdc1Z4W9pEt8UKhHcLcHn6CiaSHiyhFwRY3bMuVZwtY2XFwzQRSfW2VyYoC5Dqby43xYuEHe1Ld7ANMsXgHzwwkisdt4rQ1EHMynXM6926DEVYewgxrGEaXdYDymLWZ5DKHdmyvYW6keH9NysQGKytiax8wUyiqyaLhXoqbJCWxrcyu9MZLZpJFRXonKCNK1SAmRkRNQfkmY9jDVnj5nBdjtEGxa9pwAFENv6xa58J1zcA4MD6Tj9B4B36YrWT2TLs7Pp4jMJryA5sUVsL6jn7cEnbjaH78qQw2sicncneBaAoQnFJedyWfnQC3GYRAfRPp3kxfZNsJf2aQQ").get
-      val auctionHash = Blake2b256.hash(auctionAddr.script.bytes)
-
-      val buyBackContract = ctx.compileContract(ConstantsBuilder.create()
-        .item("CONFIG_LP", lpnft.getBytes)
-        .item("TEAM_ADDRESS", teamAddr.script.bytes)
-        .item("ACNFT", acId.getBytes)
-        .build(), Contracts.buyBack)
-      println("Buy back contract address: " + addrEnc.fromProposition(buyBackContract.getErgoTree).get)
-
-      val buyBackContractHash = Blake2b256.hash(buyBackContract.getErgoTree.bytes)
-      var contract = ctx.compileContract(ConstantsBuilder.create()
-        .item("CONFIG_LP", lpnft.getBytes)
-        .item("AuctionContractHash", auctionHash)
-        .item("BuyBackContractHash", buyBackContractHash)
-        .build(), mainContract)
-      println("Main contract address: " + addrEnc.fromProposition(contract.getErgoTree).get)
+      buyBackTest(ctx)
+      returnTest(ctx)
+      startAuctionTest(ctx)
 
     })
 
